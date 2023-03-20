11c2dbb3fd0b9eaa3c274488d3be83810bdc7fd0
Fudal96 <fudal22@gmail.com>
2023-03-20 20:52:38 +0100

    S4/39 - Using the specification methods in the controller

---
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -0,0 +1,15 @@
+using System;
+
+using Core.Entities;
+
+namespace Core.Specifications
+{
+    public class ProductsWithTypesAndBrandsSpecification : BaseSpecification<Product>
+    {
+        public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+    }
+}
