bca0f7414efa66d29038e77f84066f23fa0ddaa8
Fudal96 <fudal22@gmail.com>
2023-03-20 13:28:37 +0100

    S3/28 - Adding Seed Data

--- API/Program.cs
+++ API/Program.cs
@@ -36,6 +36,7 @@ var logger = services.GetRequiredService<ILogger<Program>>();
 try
 {
     await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
 }
 catch (Exception ex)
 {
