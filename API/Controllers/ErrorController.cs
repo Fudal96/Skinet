11c96f99623f982d34b48cfd7c49ea49ddafbd55
Fudal96 <fudal22@gmail.com>
2023-03-21 18:17:15 +0100

    S5/54 - Swagger for documenting our API

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [Route("errors/{code}")]
+    [ApiExplorerSettings(IgnoreApi = true)]
 
     public class ErrorController : BaseApiController
     {
--- API/Program.cs
+++ API/Program.cs
@@ -46,11 +46,8 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseStaticFiles();
 
