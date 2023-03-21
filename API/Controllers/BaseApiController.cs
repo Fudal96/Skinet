2312d627b2855202c2fc1afc4fdff48caed3c9a5
Fudal96 <fudal22@gmail.com>
2023-03-21 16:10:13 +0100

    S4/49|50 - Creating a test controller for errors ,- Creating a consistent error response from  the API

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
