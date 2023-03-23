60e3e29a1d0fd5313a2bd7c58c15476e3714d650
Fudal96 <fudal22@gmail.com>
2023-03-23 11:01:01 +0100

    S9/88 - Creating Shop Service

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pagination } from '../shared/models/pagination';
+import { Product } from '../shared/models/products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  getProducts() {
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products');
+  }
 }
