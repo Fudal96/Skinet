04495ed1cae1b1f9666e0e27f65bd2dd1f54dc0e
Fudal96 <fudal22@gmail.com>
2023-03-23 08:17:14 +0100

    S8/80 - Intro to the Http Client Module - Making an http request

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Skinet';
+  products: any[] = [];
+
+
+constructor(private http: HttpClient) { }
+
+
+ngOnInit(): void {
+  this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+    next: (response: any) => this.products = response.data,
+    error: error => console.log(error),
+    complete: () => {
+      console.log('request completed');
+      console.log('extra statement')
+    }
+  })
+}
+
 }
