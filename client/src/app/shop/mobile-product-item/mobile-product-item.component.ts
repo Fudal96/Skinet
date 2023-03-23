bc06ccfec933f1097cdbbfcc230e1956565168e4
Fudal96 <fudal22@gmail.com>
2023-03-23 14:32:31 +0100

    S9/91 - Adding a child component for the product items (product-item-component and mobile-product-item component) + making the view responsive

---
+++ client/src/app/shop/mobile-product-item/mobile-product-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-product-item',
+  templateUrl: './mobile-product-item.component.html',
+  styleUrls: ['./mobile-product-item.component.scss']
+})
+export class MobileProductItemComponent {
+
+}
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { MobileProductItemComponent } from './mobile-product-item/mobile-product-item.component';
 
 
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent,
+    MobileProductItemComponent
   ],
   imports: [
     CommonModule
