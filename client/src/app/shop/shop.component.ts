be33cab9fa9905bb70f1b2e937fa0597be663c35
Fudal96 <fudal22@gmail.com>
2023-03-23 20:02:28 +0100

    S9/93 - Adding the product filters

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Brand } from '../shared/models/brand';
 import { Product } from '../shared/models/products';
+import { Type } from '../shared/models/type';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,14 +11,36 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit{
 products: Product[] = [];
+brands: Brand[] = [];
+types: Type[] = [];
 
 
 constructor(private shopService: ShopService) { }
 
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+  }
+
+  getProducts() {
     this.shopService.getProducts().subscribe({
       next: response => this.products = response.data,
+      error: error => console.log(error)
+    })
+  }
+
+  getBrands() {
+    this.shopService.getBrands().subscribe({
+      next: response => this.brands = response,
+      error: error => console.log(error)
+    })
+  }
+
+  getTypes() {
+    this.shopService.getTypes().subscribe({
+      next: response => this.types = response,
       error: error => console.log(error)
     })
   }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/products';
 
@@ -14,5 +16,13 @@ export class ShopService {
 
   getProducts() {
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getBrands() {
+    return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<Type[]>(this.baseUrl + 'products/types');
   }
 }
