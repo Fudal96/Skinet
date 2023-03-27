051693c2a29246f822cc50cdaaa07f0561c4fe12
Fudal96 <fudal22@gmail.com>
2023-03-27 18:02:54 +0200

    S12/125 - Adding breadcrumbs

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
   {path: 'test-error', component: TestErrorComponent},
   {path: 'not-found', component: NotFoundComponent},
   {path: 'server-error', component: ServerErrorComponent},
