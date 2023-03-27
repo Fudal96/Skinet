c4f3dae5c815bfd76e54beea686b13da45590339
Fudal96 <fudal22@gmail.com>
2023-03-27 14:07:20 +0200

    S10/113 - Lazy loading angular modules

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { FormsModule } from '@angular/forms';
 import { HomeModule } from './home/home.module';
 
@@ -21,7 +20,6 @@ import { HomeModule } from './home/home.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
     FormsModule,
     HomeModule
   ],
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -5,7 +5,7 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import { MobileProductItemComponent } from './mobile-product-item/mobile-product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { RouterModule } from '@angular/router';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
@@ -19,10 +19,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule
-  ],
-  exports: [
-    ShopComponent
+    ShopRoutingModule
   ]
 })
 export class ShopModule { }
